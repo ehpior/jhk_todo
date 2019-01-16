9c147d0d9a2c353d42dd576529c8e795d037ce7d
ehpior <ehpior@kookmin.ac.kr>
2019-01-16 15:34:43 +0900

    create models & urls

--- todo/models.py
+++ todo/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Ttodo(models.Model):
+    title = models.CharField(max_length = 20)
+    content = models.TextField()
+    deadline = models.DateField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
---
+++ todo/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'todo'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:todo_id>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('modify/', views.modify, name='modify'),
+    path('delete/', views.delete, name='delete'),
+]
