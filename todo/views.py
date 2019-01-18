d59037fd71636c67db30c67c04546852973ea722
ehpior <ehpior@kookmin.ac.kr>
2019-01-18 15:11:31 +0900

    detail, modify, delete 기능 완료

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from todo.models import Ttodo
-from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-
 
 def index(request):
     todo_list = Ttodo.objects.all()
@@ -17,8 +15,8 @@ def index(request):
     return render(request, 'todo/index.html', {'todo_fin': todo_fin,'todo_not': todo_not})
 
 def detail(request,todo_id):
-    todo_content = get_object_or_404(Ttodo, key=todo_id)
-    return render(request, 'todo/detail.html', {'todo_content': todo_content})
+    todo = get_object_or_404(Ttodo, pk=todo_id)
+    return render(request, 'todo/detail.html', {'todo': todo})
 
 def create(request):
     if request.method == "POST":
@@ -31,12 +29,18 @@ def create(request):
     return render(request, 'todo/create.html')
 
 def modify(request,todo_id):
-    todo_content = get_object_or_404(Ttodo, key=todo_id)
-    
-    return render(request, 'todo/modify.html', {'todo_content': todo_content})
+    todo = get_object_or_404(Ttodo, pk=todo_id)
+    if request.method == "POST":
+        k = request.POST
+        if k['deadline']=='':
+            Ttodo.objects.filter(pk=todo_id).update(title=k['title'],content=k['content'])
+        else:
+            Ttodo.objects.filter(pk=todo_id).update(title=k['title'],content=k['content'],deadline=k['deadline'])
+        return HttpResponseRedirect(reverse('todo:detail',kwargs={'todo_id': todo_id}))
+    return render(request, 'todo/modify.html', {'todo': todo})
 
 def delete(request,todo_id):
-    todo_content = get_object_or_404(Ttodo, key=todo_id)
-    
-    return render(request, 'todo/delete.html', {'todo_content': todo_content})
+    todo = get_object_or_404(Ttodo, pk=todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('todo:index'))
 
