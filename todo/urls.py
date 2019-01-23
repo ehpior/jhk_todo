64187608211e5b24756879be07911b3e505a726c
ehpior <ehpior@kookmin.ac.kr>
2019-01-23 16:33:24 +0900

    change

--- todo/urls.py
+++ todo/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('complete/', views.complete, name='complete'),
     path('<int:todo_id>/', views.detail, name='detail'),
     path('<int:todo_id>/modify/', views.modify, name='modify'),
+    path('complete/<int:todo_id>/modify/', views.modify, name='modify'),
     path('<int:todo_id>/delete/', views.delete, name='delete'),
+    path('complete/<int:todo_id>/delete/', views.delete, name='delete'),
     path('<int:todo_id>/complete/', views.make_complete, name='make_complete')
 ]
